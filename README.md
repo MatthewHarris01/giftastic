199795cd6280ae932397b8fb9b8c2ee41cef88c2
Matthew Harris <simonh241@yahoo.com>
2020-01-09 21:16:14 -0800

    Version 0.1 -- See Readme.md

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # giftastic
 A fun app to select GIF files from GIPHY.com, on topics entered by the user.
 Version 0.0 -- repository created, placeholder files for style.css, app.js, and index.html created. no code or html content yet.
+Version 0.1 -- code to load topics and favorites from local storage completed and tested, also the code to render search buttons from the list of topic is complete and working. Index.html ostly complete, still has some controls to add.
 
-NEXT TO DO: design and implement the index.jtml page and start defining styles as needed.
+NEXT TO DO: 1) implent code to make ajax call to GIPHY  API, and display results.
+2) Add contols to enable user how many results to return per search, 5, 10, 15, or 20.
+3) Add controls to enable user to select what ratings to return - R, PG-13, G or any.
+4) add controls to enable user to clear the search results section.
+5) implement doubl-click on a gif image to add it to favorites.
+6) implement double-click on a favorite gif to remove it from favorites
+7) implement right-click to delete a topic button and its associated local storage key.
+7) implement code to delete all local storage entries for favorites and topics, since those get re-written in the course of the app's operation.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,234 @@
 // Java Script for GIFTatic app, started 1/7 1O:59 pm
+
+var defaultgiftopics = ["cat", "dog", "ostrich"]; //default topics if there are none in local storage
+var topiclist = []; //list of topics if created from local storage, or as added to by user.
+var favelist = [];  //list of urls marked as favorites  and retrieved from local storage.
+
+//*********************************************************************************
+// RENDER BUTTONS
+//*********************************************************************************
+    // Function for displaying gif topic buttons data
+    function renderButtons() {
+      console.log("inside renderButtons function");
+
+    // Deleting the topics prior to adding new topics
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#buttons-view").empty();
+
+    // return; //FOR TESTING
+
+    // Looping through the array of topics
+    console.log("there are " + topiclist.length + " buttons to add");
+    for (var i = 0; i < topiclist.length; i++) {
+
+    // Then dynamicaly generating buttons for each topid in the array
+    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    var a = $("<button>");
+    // Adding a class of topic-btn to our button
+    a.addClass("topic-btn");
+    // Adding a data-attribute
+    a.attr("data-name", topiclist[i]);
+    console.log("adding topic: " + topiclist[i]);
+    // Providing the initial button text
+    a.text(topiclist[i]);
+    // Adding the button to the buttons-view div
+    $("#buttons-view").append(a);
+        }
+      console.log("end of renderButtons function");
+      } //end of renderButtons function
+
+
+//*************************************************************
+// LOAD TOPICS
+//*************************************************************
+function loadtopics() {
+  //this function loads topics from local storage
+  console.log("inside loadtopics function");
+
+  // console.log("localstorage elements to process: " + window.localStorage.length);
+
+  //iterate all keys a
+  var tempkeys = [];
+  for (i=0; i < window.localStorage.length; i++) {
+    console.log(localStorage.key(i));
+    tempkeys.push(localStorage.key(i));
+  }
+
+  // console.log("KEY ARRAY IS: " + tempkeys);
+
+    //now process the array of key values, and save only those keys that begin with "giftastic"
+    var tempkeys2 = []  // temp array to hold keys that we want
+    for (i=0; i<tempkeys.length; i++) {
+      if (tempkeys[i].search("giftastic") == 0) {
+        //if the search result is 0, then the key name starts with "gigtastic"
+        // console.log("we want this key: "+ tempkeys[i]);
+       tempkeys2.push(tempkeys[i]);
+      }
+     else {
+      //  console.log("we do NOT WANT this key: " + tempkeys[i]);
+     }
+// console.log("array of keys we want: " + tempkeys2);
+    }
+      
+    //sort the array of key values we want
+    // console.log("UNSORTED ARRAY: " + tempkeys2);
+    tempkeys2.sort();
+    // console.log("SORTED ARRAY of keys we want: " + tempkeys2);
+
+    //check the content of tempkeys2
+    // console.log("TESTING CONTENT OF TEMPKEYS2");
+    for (i=0; i< tempkeys2.length; i++) {
+      console.log(tempkeys2[i]);
+    }
+       //now iterate the array of giftastic keys, and fetch their values into the topic list attay
+       var temptopics = []; //temp arrary to hold topics values
+       var temptopic = "";  //temp var to hold key value being retrieved
+       for (i=0; i < tempkeys2.length; i++) {
+         temptopic = localStorage.getItem(tempkeys2[i]);
+        //  console.log("key being retrived: "+ tempkeys2[i] );
+        //  console.log("KEY VALUE: " + temptopic);
+         temptopics.push(temptopic);
+       }
+    
+      //  console.log("TEMPTOPICS   ARRAY: " + temptopics);
+
+
+       //if the temptopics array is empty (no giftastic keys found), use the default topics
+       if (temptopics.length == 0) {
+        //  console.log("USE DEFAULTS");
+         topiclist = defaultgiftopics;
+       }
+       else {
+        //  console.log("USE CONTENT OF TEMPTOPICS ARRAY");
+         topiclist = temptopics;
+       }
+
+       //make one final check
+      //  console.log("TEST TOPICLIST ARRAY");
+       for (i = 0; i< topiclist.length; i++) {
+          // console.log(topiclist[i]);
+
+       }
+
+
+
+  console.log("end of loadtopic function");
+} // end of loadtopics function
+
+//*********************************************************************************
+// LOAD FAVORITES
+//*********************************************************************************
+function loadfavorites() {
+  //this function loads  favorit urls from local storage
+  console.log("inside loadfavoties function");
+
+  console.log("localstorage elements to process: " + window.localStorage.length);
+
+  //iterate all keys a
+  var tempkeys = [];
+  for (i=0; i < window.localStorage.length; i++) {
+    console.log(localStorage.key(i));
+    tempkeys.push(localStorage.key(i));
+  }
+
+  console.log("KEY ARRAY IS: " + tempkeys);
+
+    //now process the array of key values, and save only those keys that begin with "giffave"
+    var tempkeys2 = []  // temp array to hold keys that we want
+    for (i=0; i<tempkeys.length; i++) {
+      if (tempkeys[i].search("giffave") == 0) {
+        //if the search result is 0, then the key name starts with "giffave"
+        console.log("we want this key: "+ tempkeys[i]);
+       tempkeys2.push(tempkeys[i]);
+      }
+     else {
+       console.log("we do NOT WANT this key: " + tempkeys[i]);
+     }
+console.log("array of keys we want: " + tempkeys2);
+    }
+      
+    //sort the array of key values we want
+    console.log("UNSORTED ARRAY: " + tempkeys2);
+    tempkeys2.sort();
+    console.log("SORTED ARRAY of keys we want: " + tempkeys2);
+
+    //check the content of tempkeys2
+    console.log("TESTING CONTENT OF TEMPKEYS2");
+    for (i=0; i< tempkeys2.length; i++) {
+      console.log(tempkeys2[i]);
+    }
+       //now iterate the array of giffave keys, and fetch their values into the favorites list attay
+       var tempfaves = []; //temp arrary to hold favorites values
+       var tempfave = "";  //temp var to hold key value being retrieved
+       for (i=0; i < tempkeys2.length; i++) {
+         tempfave = localStorage.getItem(tempkeys2[i]);
+         console.log("key being retrived: "+ tempkeys2[i] );
+         console.log("KEY VALUE: " + tempfave);
+         tempfaves.push(tempfave);
+       }
+    
+       console.log("TEMPFAVES   ARRAY: " + tempfaves);
+
+
+      //  //if the temptopics array is empty (no giftastic keys found), use the default topics
+      //  if (temptopics.length == 0) {
+      //    console.log("USE DEFAULTS");
+      //   //  topiclist = defaultgiftopics;
+      //  }
+       
+         console.log("USE CONTENT OF TEMPFAVES ARRAY to populate the favelist array");
+         favelist = tempfaves;
+       
+
+       //make one final check
+       console.log("TEST FAVES ARRAY");
+       for (i = 0; i< favelist.length; i++) {
+          console.log(favelist[i]);
+
+       }
+  console.log("end of loadfavorites function");
+} // end of loadfavorites function
+
+
+
+//*********************************************************************************
+
+$(document).ready(function()  {
+  console.log("document is ready function");
+
+  console.log("inside document ready function");
+
+  // Sorttest();
+localStorage.setItem("fluff", "expected over-write");
+
+// localStorage.removeItem("fluff");
+
+  // return;
+
+    //see if there are any topics to load from local storage
+    var keycount = window.localStorage.length;
+
+    if (keycount > 0) {
+      console.log("there are possibly topics adn favorites to load from local storage");
+      loadtopics();
+      loadfavorites();
+      renderButtons();
+    }
+    else {
+      //no topics to load, use the contents of defaulgiftopics to get started
+      console.log("NO TOPICS TO LOAD");
+      topiclist = defaultgiftopics;
+      renderButtons();
+    }
+
+  // console.log("local storage length: " + window.localStorage.length); //check number of local storage entries
+
+  // console.log("first local storage item: " + localStorage.key(1));
+  // var key = localStorage.key(1);
+  // console.log("value of local storage key 1 " + localStorage.getItem(key));
+
+
+
+  console.log("end of document ready function");
+} //end of document ready function
+) //end of document ready evant handler parameter list
