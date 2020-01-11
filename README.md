537070e4192b32a6d8ff0df5acb43a6d89be92f9
Matthew Harris <simonh241@yahoo.com>
2020-01-10 23:13:08 -0800

    Version 0.3 -- See Readme.md

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # giftastic
 A fun app to select GIF files from GIPHY.com, on topics entered by the user.
 Version 0.0 -- repository created, placeholder files for style.css, app.js, and index.html created. no code or html content yet.
-<<<<<<< HEAD
 Version 0.1 -- code to load topics and favorites from local storage completed and tested, also the code to render search buttons from the list of topic is complete and working. Index.html mostly complete, still has some controls to add. css tyles started.
 Version 0.2 -- added drop-down lists so that user may select the rating and limit of gifs returned by search.
+Version 0.3 -- implemented code to make the ajax call to the GIPHY API, and display results. user choice of rating and number of results is completely implemented and tested. A button to clear the search results div has been implemented and tested.
 
-NEXT TO DO: 1) implement code to make ajax call to GIPHY  API, and display results.
-2) add controls to enable user to clear the search results section.
-3) implement double-click on a gif image to add it to favorites.
-4) implement double-click on a favorite gif to remove it from favorites
-5) implement right-click to delete a topic button and its associated local storage key.
-6) implement code to delete all local storage entries for favorites and topics, since those get re-written in the course of the app's operation.
+NEXT TO DO: 1) implement double-click on a gif image to add it to favorites.
+2) implement double-click on a favorite gif to remove it from favorites
+3) implement right-click to delete a topic button and its associated local storage key.
+4) implement code to delete all local storage entries for favorites and topics, since those get re-written in the course of the app's operation.
 =======
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,39 @@ var favelist = [];  //list of urls marked as favorites  and retrieved from local
 var newTopic = "";  //to hold new topic before it is added to topiclist array.
 
 //*********************************************************************************
+// RENDER IMAGE
+//*******************************************************************************
+function renderImage(url,alt,classname, num) {
+  //this function is an experiment to see if using a distinct function to add an image tag to the results dive works
+  //better than doing it in the loop that processes all the images -- for some reason, only the last image rendered in the 
+  //loop appears in the results div, which seems very odd -- and also, it only appears if ther is already (even if empty) and image tag
+  //somewhere on the page. I really don't get why this hasn't been working. The other thing is that my the new image my code is supposedly adding takes the place of any existing image tag on the page -- like the GIPHY  attribution image.!!!!
+  //weird shit, man.
+  var rsltimg;
+  console.log("inside render image function")
+
+  console.log("image url: " + url)
+  console.log("image alt: " + alt);
+  console.log("image class name: " + classname);
+  console.log("image number: " + num);
+    rsltimg = $("<img>");
+    rsltimg.addClass(classname);
+    rsltimg.attr("src", url);
+    rsltimg.attr("alt", alt);
+    rsltimg.attr("alt", "image " + num);
+    rsltimg.attr("data-name", num);
+
+console.log("appending image");
+    $("#imgresults").append(rsltimg);
+
+    // $("#imgresults").append(url);
+    // var img = $("<img>");
+
+    console.log("end of render image function");
+}
+
+
+//*********************************************************************************
 // RENDER BUTTONS
 //*********************************************************************************
     function renderButtons(newbtn) {
@@ -19,15 +52,17 @@ var newTopic = "";  //to hold new topic before it is added to topiclist array.
     // (this is necessary otherwise you will have repeat buttons)
     $("#buttons-view").empty();
 
-    console.log("BUTTON TO ADD: [" + newbtn + "]");
+    // console.log("BUTTON TO ADD: [" + newbtn + "]");
+
     // add new button to topiclist array
     if (newbtn.length > 0) {
-      //only push to the topiclist array if the newbtn parameter is not empy
+      //only push to the topiclist array if the newbtn parameter is not empty
       topiclist.push(newbtn);
     }
 
     // Looping through the array of topics
-    console.log("there are " + topiclist.length + " buttons to add");
+    // console.log("there are " + topiclist.length + " buttons to add");
+
     for (var i = 0; i < topiclist.length; i++) {
 
     // Then dynamicaly generating buttons for each topic in the array
@@ -37,7 +72,7 @@ var newTopic = "";  //to hold new topic before it is added to topiclist array.
     a.addClass("topic-btn");
     // Adding a data-attribute
     a.attr("data-name", topiclist[i]);
-    console.log("adding topic: " + topiclist[i]);
+    // console.log("adding topic: " + topiclist[i]);
     // Providing the initial button text
     a.text(topiclist[i]);
     // Adding the button to the buttons-view div
@@ -46,14 +81,16 @@ var newTopic = "";  //to hold new topic before it is added to topiclist array.
 
     //write topiclist array into local storage, if needed
     if (newbtn.length > 0) {
-      console.log("WRITE TOPICLIST TO LOCAL STORAGE!!");
+      
+      // console.log("WRITE TOPICLIST TO LOCAL STORAGE!!");
+
       var newkey = "giftastic"
       var key = "";
       for (i=0; i < topiclist.length; i++) {
         //iterate topic list, generate a key, and write new key:value pair to local storage.
         key = newkey + i;
-        console.log("NEW KEY: " + key);
-        console.log("VALUE: " + topiclist[i]);
+        // console.log("NEW KEY: " + key);
+        // console.log("VALUE: " + topiclist[i]);
         localStorage.setItem(key, topiclist[i]);
       }
 
@@ -115,7 +152,7 @@ function loadtopics() {
 //*********************************************************************************
 function loadfavorites() {
   //this function loads  favorit urls from local storage
-  console.log("inside loadfavoties function");
+  console.log("inside loadfavorites function");
 
   //iterate all keys a
   var tempkeys = [];
@@ -156,17 +193,19 @@ function loadfavorites() {
 
 
        
-         console.log("USE CONTENT OF TEMPFAVES ARRAY to populate the favelist array");
+        //  console.log("USE CONTENT OF TEMPFAVES ARRAY to populate the favelist array");
          favelist = tempfaves;
        
 
        //make one final check
-       console.log("TEST FAVES ARRAY");
-       for (i = 0; i< favelist.length; i++) {
-          console.log(favelist[i]);
+      //  console.log("TEST FAVES ARRAY");
+      //  for (i = 0; i< favelist.length; i++) {
+      //     console.log(favelist[i]);
 
-       }
-  console.log("end of loadfavorites function");
+      //  }
+
+
+      console.log("end of loadfavorites function");
 } // end of loadfavorites function
 
 
@@ -205,8 +244,8 @@ $(document).ready(function()  {
         var topic = $("#gif-input").val().trim();
         newTopic = topic.toLowerCase();
 
-        console.log("ADD TOPIC BUTTON CLICKED");
-        console.log("NEW TOPIC IS: [" + newTopic + "]");
+        // console.log("ADD TOPIC BUTTON CLICKED");
+        // console.log("NEW TOPIC IS: [" + newTopic + "]");
 
         // Calling renderButtons which handles the processing of our topiclist array
         renderButtons(newTopic);
@@ -239,38 +278,150 @@ $(document).ready(function()  {
         console.log("SELECTED LIMIT IS: " + limit);
         console.log("SELECTED RATING IS: " + rating);
 
-
-        //SAMPLE API
-//        https://api.giphy.com/v1/gifs/search?api_key=1EMx5NFcJ1QPK7RBP6kFYQgbwHvl3YOF&q=&limit=20&offset=0&rating=PG&lang=en  
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=1EMx5NFcJ1QPK7RBP6kFYQgbwHvl3YOF&q=" + searchFor
         
-//        https://api.giphy.com/v1/gifs/search?api_key=1EMx5NFcJ1QPK7RBP6kFYQgbwHvl3YOF&q=CATS&limit=20&offset=0&rating=PG&lang=en      
+        if (rating === "any") {
+          console.log("RATING IS ANY");
+          queryURL = queryURL + "&limit=" + limit + "offset=0&lang=en"
+        }
+        else {
+          queryURL = queryURL + "&limit=" + limit + "offset=0&rating=" + rating + "&lang=en"
+        }
 
-//note the 'q' parameter (cats in this example), the limit parameter
-//https://api.giphy.com/v1/gifs/search?api_key=1EMx5NFcJ1QPK7RBP6kFYQgbwHvl3YOF&q=CATS&limit=20&offset=0&rating=PG&lang=en  
+        // console.log("URL: " + queryURL);
 
-        //set up queryURL and make ajax call to GIPHY API here
-        // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=[search term]&api_key=1EMx5NFcJ1QPK7RBP6kFYQgbwHvl3YOF&limit=5");
-        // xhr.done(function(data) { console.log("success got data", data); });
-        
-        // the api url below gets random gifs with the tag=cats.
-        // var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=cats";
-        
-        // var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+        // Creating an AJAX call for the specific gif search crieteria 
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+          
+          console.log(response);
 
-        // // Creating an AJAX call for the specific movie button being clicked
-        // $.ajax({
-        //   url: queryURL,
-        //   method: "GET"
-        // }).then(function(response) {
+          console.log("iterating response data below");
+          console.log("THERE ARE: " + response.data.length + " IMAGES IN THE RESULTS");
+          var imglist = [];
+          for (i=0; i < response.data.length; i++) {
+            //iterate through response data and get gif info
+          // console.log("GIF NUMBER: " + i)
+            var giftitle = response.data[i].title;
+            var gifrating = response.data[i].rating
+            var username = response.data[i].username;
+            if (username == "" ) {
+              //usernme is empty
+              username = "not provided"
+            }
+            //now make one big string with all the info about the gif in it
+            // sInfo = "Title: " + giftitle + "<br>Rating: " + gifrating + "<br>"
+
+
+          // console.log("gif title: " + response.data[i].title);
+          // console.log("gif rating: " + response.data[i].rating);
+          console.log("gif username: [" + response.data[i].username + "]");
+
+            //create a new div to hold the gif image:
+            console.log("make new div to hold gif info and image");
+            var rslt_div = $("<div>")
+            rslt_div.addClass("gifresult");
+            
+
+            //now make a p tag with the gif title
+            var Ptitle = $("<p>").text("title: " + giftitle);
+            Ptitle.addClass("gifinfo");
+            
+            //now append the title p tag to the result div.
+            console.log("adding Ptitle to rslt_div");
+            rslt_div.append(Ptitle);
+
+
+            //make the p tag with gif rating
+            var Prating = $("<p>").text("Rating: " + gifrating);
+            Prating.addClass("gifinfo");
+            //append the rating to the result div.
+            console.log("adding Prating to rslt_div");
+            rslt_div.append(Prating);
+
+            //now add the username info
+            var Pusername = $("<p>").text("User name: " + username);
+            Pusername.addClass("gifinfo");
+
+            //append the username info
+            console.log("adding Pusername to rslt_div")
+           rslt_div.append(Pusername);
+
+            //finally make the img tag for the gif
+          console.log("making the image tag for the gif");
+            // console.log("adding image: " + i);
+            var rsltimg = $("<img>");
+            rsltimg.addClass("gif-result");
+            rsltimg.attr("src", response.data[i].images.fixed_height.url);
+            rsltimg.attr("alt", searchFor + " image");
+            
+            
 
 
 
-        console.log("end of add topic button click");
-      } //end of search button click handler
-      );
+          //   console.log("RESULT IMAGE URL: " + response.data[i].images.fixed_height.url);
+
+            //now, append the gif into the rslt div
+            console.log("adding rslt_img to rslt_div");
+            rslt_div.append(rsltimg);
+
+
+         
+            //finally prepend the entire result div into the main results div
+            console.log("adding img to results div");
+            $("#imgresults").prepend(rslt_div);
+         
+
+
+          }  //end of for loop for all results
+
+          } //end of ajax response processing
+        );  //end of search button event parameter list
+
+
+      console.log("end of search button click handler");
+} //end of search button click function
+) //end of search button click parameter list
+
+//*********************************************************************************
+// CLEAR RESULT BUTTON CLICK
+//*********************************************************************************
+
+
+$("#clearresults").on("click", function( ) {
+  //This event function empties the search results div.
+
+  console.log("CLEAR RESULTS BUTTON CLICKED");
+
+  $("#imgresults").empty();
 
 
 
-  console.log("end of document ready function");
-} //end of document ready function
-) //end of document ready evant handler parameter list
+  console.log("end of clear results button click function")
+} //end of clear result button click function
+
+) //end of clear result button click event
+
+
+//*********************************************************************************
+// RESULT IMAGE DOUBLECLICK
+//*********************************************************************************
+$(document).on("click", ".gifresult",  function(event) {
+  // $(".gifresult").on("click", function () {
+
+    console.log("RESULT IMAGE DOUBLE-CLICKED");
+
+    var gifdiv = this
+
+    console.log("prove that I now have a handle on the right gif")
+
+    console.log(gifdiv)
+
+} //end of doubleclick event function for rresult gif
+) //end of doubleclick event listeneer for reseult gif
+
+console.log("end of document ready function");
+} //end of document ready function 
+) //end of document read patameter list
