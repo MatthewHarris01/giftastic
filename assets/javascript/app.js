aec373dfa685c1304f0a270569146d30a50972b8
Matthew Harris <simonh241@yahoo.com>
2020-01-12 22:55:38 -0800

    Version 0.6 -- See Readme.md

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,17 @@ var topiclist = []; //list of topics if created from local storage, or as added 
 var favelist = [];  //list of urls marked as favorites  and retrieved from local storage.
 var newTopic = "";  //to hold new topic before it is added to topiclist array.
 
+//*********************************************************************************
+// RENDER BUTTONS
+//*********************************************************************************
+function playAPIdone() {
+  //this function plays the sound for when API results are returned
+  console.log("inside playAPIdone function");
+  var snd = document.getElementById("myAudio");
+  snd.play();
 
+  console.log("end playAPIdone function");
+}
 
 //*********************************************************************************
 // RENDER BUTTONS
@@ -58,15 +68,16 @@ var newTopic = "";  //to hold new topic before it is added to topiclist array.
     //write topiclist array into local storage, if needed
     if (newbtn.length > 0) {
       
-      // console.log("WRITE TOPICLIST TO LOCAL STORAGE!!");
+      console.log("WRITE TOPICLIST TO LOCAL STORAGE!!");
 
       var newkey = "giftastic"
       var key = "";
       for (i=0; i < topiclist.length; i++) {
         //iterate topic list, generate a key, and write new key:value pair to local storage.
         key = newkey + i;
-        // console.log("NEW KEY: " + key);
-        // console.log("VALUE: " + topiclist[i]);
+        console.log("NEW KEY: " + key);
+        console.log("VALUE: " + topiclist[i]);
+        console.log("next line should write out the key to local storage")
         localStorage.setItem(key, topiclist[i]);
       }
 
@@ -196,6 +207,7 @@ $(document).ready(function()  {
   console.log("inside document ready function");
 
 
+
     //see if there are any topics to load from local storage
     var keycount = window.localStorage.length;
 
@@ -224,8 +236,8 @@ $(document).ready(function()  {
 
         newTopic = topic.toLowerCase();
 
-        console.log("ADD TOPIC BUTTON CLICKED");
-        console.log("NEW TOPIC IS: [" + newTopic + "]");
+        // console.log("ADD TOPIC BUTTON CLICKED");
+        // console.log("NEW TOPIC IS: [" + newTopic + "]");
 
         // Calling renderButtons which handles the processing of our topiclist array
         renderButtons(newTopic);
@@ -249,19 +261,19 @@ $(document).ready(function()  {
         // var topic = $("#gif-input").val().trim();
         // newTopic = topic.toLowerCase();
 
-        console.log("SEARCH TOPIC BUTTON CLICKED");
-        console.log("TOPIC TO SEARCH FOR IS: [" + searchFor + "]");
+        // console.log("SEARCH TOPIC BUTTON CLICKED");
+        // console.log("TOPIC TO SEARCH FOR IS: [" + searchFor + "]");
 
         var limit = $("#limit").val();
         var rating = $("#rating").val();
 
-        console.log("SELECTED LIMIT IS: " + limit);
-        console.log("SELECTED RATING IS: " + rating);
+        // console.log("SELECTED LIMIT IS: " + limit);
+        // console.log("SELECTED RATING IS: " + rating);
 
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=1EMx5NFcJ1QPK7RBP6kFYQgbwHvl3YOF&q=" + searchFor
         
         if (rating === "any") {
-          console.log("RATING IS ANY");
+          // console.log("RATING IS ANY");
           queryURL = queryURL + "&limit=" + limit + "offset=0&lang=en"
         }
         else {
@@ -276,19 +288,19 @@ $(document).ready(function()  {
           method: "GET"
         }).then(function(response) {
           
-          console.log(response);
+          // console.log(response);
 
-          console.log("iterating response data below");
-          console.log("THERE ARE: " + response.data.length + " IMAGES IN THE RESULTS");
+          // console.log("iterating response data below");
+          // console.log("THERE ARE: " + response.data.length + " IMAGES IN THE RESULTS");
           var imglist = [];
           for (i=0; i < response.data.length; i++) {
             //iterate through response data and get gif info
           // console.log("GIF NUMBER: " + i)
             var gifid = response.data[i].id;
             var gifStillurl = response.data[i].images.original_still.url;
-console.log("ORIGINAL STILL URL: " + response.data[i].images.original_still.url);            
+// console.log("ORIGINAL STILL URL: " + response.data[i].images.original_still.url);            
             var gifAnimurl = response.data[i].images.original.url;
-console.log("ORIGINAL ANIM URL" + response.data[i].images.original.url)            ;
+// console.log("ORIGINAL ANIM URL" + response.data[i].images.original.url)            ;
             var giftitle = response.data[i].title;
             var gifrating = response.data[i].rating
             var username = response.data[i].username;
@@ -296,16 +308,14 @@ console.log("ORIGINAL ANIM URL" + response.data[i].images.original.url)         
               //usernme is empty
               username = "not provided"
             }
-            //now make one big string with all the info about the gif in it
-            // sInfo = "Title: " + giftitle + "<br>Rating: " + gifrating + "<br>"
 
 
           // console.log("gif title: " + response.data[i].title);
           // console.log("gif rating: " + response.data[i].rating);
-          console.log("gif username: [" + response.data[i].username + "]");
+          // console.log("gif username: [" + response.data[i].username + "]");
 
             //create a new div to hold the gif image:
-            console.log("make new div to hold gif info and image");
+            // console.log("make new div to hold gif info and image");
             var rslt_div = $("<div>");
             rslt_div.attr("id", gifid);    //set result div id to be the gif id
             rslt_div.addClass("gifresult");
@@ -321,7 +331,7 @@ console.log("ORIGINAL ANIM URL" + response.data[i].images.original.url)         
             Ptitle.addClass("gifinfo");
             
             //now append the title p tag to the result div.
-            console.log("adding Ptitle to rslt_div");
+            // console.log("adding Ptitle to rslt_div");
             rslt_div.append(Ptitle);
 
 
@@ -329,7 +339,7 @@ console.log("ORIGINAL ANIM URL" + response.data[i].images.original.url)         
             var Prating = $("<p>").text("Rating: " + gifrating);
             Prating.addClass("gifinfo");
             //append the rating to the result div.
-            console.log("adding Prating to rslt_div");
+            // console.log("adding Prating to rslt_div");
             rslt_div.append(Prating);
 
             //now add the username info
@@ -337,14 +347,14 @@ console.log("ORIGINAL ANIM URL" + response.data[i].images.original.url)         
             Pusername.addClass("gifinfo");
 
             //append the username info
-            console.log("adding Pusername to rslt_div")
+            // console.log("adding Pusername to rslt_div")
            rslt_div.append(Pusername);
 
 
             //finally make the img tag for the gif
-          console.log("making the image tag for the gif");
-console.log("STILL url: [" + gifStillurl) + "]";
-console.log("ANIM url: [" + gifAnimurl + "]");
+          // console.log("making the image tag for the gif");
+// console.log("STILL url: [" + gifStillurl) + "]";
+// console.log("ANIM url: [" + gifAnimurl + "]");
                       // console.log("adding image: " + i);
             var rsltimg = $("<img>");
             rsltimg.addClass("gif-result");
@@ -356,15 +366,16 @@ console.log("ANIM url: [" + gifAnimurl + "]");
             rsltimg.attr("data-url-anim",gifAnimurl);
             
             //now, append the gif into the rslt div
-            console.log("adding rslt_img to rslt_div");
+            // console.log("adding rslt_img to rslt_div");
             rslt_div.append(rsltimg);
 
             //finally prepend the entire result div into the main results div
-            console.log("adding img to results div");
+            // console.log("adding img to results div");
             $("#imgresults").prepend(rslt_div);
 
           }  //end of for loop for all results
 
+            console.log("end of ajax rsponse processing");
           } //end of ajax response processing
         );  //end of search button event parameter list
 
@@ -374,7 +385,22 @@ console.log("ANIM url: [" + gifAnimurl + "]");
 ) //end of search button click parameter list
 
 //*********************************************************************************
-// CLEAR RESULT BUTTON CLICK
+// RIGHT-CLICK TOPIC SEARCH BUTTON
+//*********************************************************************************
+$(document).on("contextmenu", "#buttons-view",  function(event) {
+  //this function handles the reight-click event for topic search buttons
+
+
+  console.log("RIGHT-CLICK EVENT ON TOPIC BUTTON");
+  // $('#fBox' + folderID).on("contextmenu", function(event) {
+
+} //end of mousedown event listener function
+) //end of mousedown event listener
+
+
+
+//*********************************************************************************
+// CLEAR IMAGE RESULTS BUTTON CLICK
 //*********************************************************************************
 
 
@@ -397,7 +423,8 @@ $("#clearresults").on("click", function( ) {
 // RESULT IMAGE DOUBLECLICK
 //*********************************************************************************
 $(document).on("dblclick", ".gifresult",  function(event) {
-  // $(".gifresult").on("click", function () {
+  event.preventDefault(); //PREVENT DEFAULT ACTION
+  event.stopPropagation ();
 
     console.log("RESULT div DOUBLE-CLICKED");
 
@@ -417,39 +444,39 @@ $(document).on("dblclick", ".gifresult",  function(event) {
 $(document).on("click", ".gif-result",  function(event) {
   // $(".gifresult").on("click", function () {
 
-    console.log("RESULT img CLICKED --TURN ANIMATION ON");
+    console.log("RESULT img CLICKED --TURN ANIMATION ON/OFF");
 
     var gifimg = $(this);
 
-    console.log("prove that I now have a handle on the right IMAGE")
+    // console.log("prove that I now have a handle on the right IMAGE")
 
-    console.log(gifimg);
+    // console.log(gifimg);
 
-    console.log("image ID: " + $(this).attr("id"));
+    // console.log("image ID: " + $(this).attr("id"));
 
     var imgState = $(this).attr("data-state");
 
-    console.log("image data-state: " + imgState);
+    // console.log("image data-state: " + imgState);
 
     if (imgState == "still") {
       //change data-state attribute
       $(this).attr("data-state", "anim");
       //now use data-url-anim attribute to set the src attribute
       var temp = $(this).attr("data-url-anim");
-      console.log("ANIMATED URL IS: " + temp);
+      // console.log("ANIMATED URL IS: " + temp);
       $(this).attr("src", temp);
 
-      console.log("GIF SHOULD NOW BE  ANIMATED")
+      // console.log("GIF SHOULD NOW BE  ANIMATED")
     }
     else {
       //set the data-state back to still
       $(this).attr("data-state", "still");
       //use the data-url-still attribute to set the src attribut
       var temp = $(this).attr("data-url-still");
-      console.log("STILL URL IS: " + temp);
+      // console.log("STILL URL IS: " + temp);
       $(this).attr("src", temp);
 
-      console.log("GIF SHOULD NOW BE  STILL");
+      // console.log("GIF SHOULD NOW BE  STILL");
     }
 
 
